f9565347cf5d0aecb5e43e7c1e8f76469de99e64
Rakesh7502 <93503869+Rakesh7502@users.noreply.github.com>
2024-10-21 21:51:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ const dbUrl=process.env.ATLASDB_URL;
 main()
   .then(() => {
     console.log("connected to DB");
+    //console.log("connection successfull");
   })
   .catch((err) => {
     console.log(err);
